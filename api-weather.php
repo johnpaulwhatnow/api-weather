3fdad1b41f5d4577ce0bbe0cafd63f99bb03deb5
johnpaulwhatnow <john@johnpaulwhatnow.com>
2016-11-11 20:41:21 -0600

    fixed encoding problem

--- api-weather.php
+++ api-weather.php
@@ -11,24 +11,24 @@ License: GPL2
 */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
     die;
 }
 
 /*--------------------------------------------*
 * Plugin Configuration
 *--------------------------------------------*/
-$accuweather_api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+$accuweather_api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
 //load the needed files.  This plugin calls two classes directly. Both are included below.
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-curl-wrapper.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-api-weather.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-curl-wrapper.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-api-weather.php';
 
 //Here we get the root of the plugin. This is used for css and imgs, and is explained more in includes/class-api-weather.php.
-$plugin_dir_url_root = plugin_dir_url( __FILE__ );
+$plugin_dir_url_root = plugin_dir_url(__FILE__);
 
 // instantiate plugin's class
-$ApiWeather = new ApiWeather( new CurlWrapper( ), $plugin_dir_url_root, $accuweather_api_key );
+$ApiWeather = new ApiWeather(new CurlWrapper(), $plugin_dir_url_root, $accuweather_api_key);
 
 //make the class globally availalble.
 $GLOBALS['api_weather'] = $ApiWeather;
@@ -38,7 +38,7 @@ $GLOBALS['api_weather'] = $ApiWeather;
 add_filter('widget_text', 'do_shortcode');
 
 //add this plugin's only shortcode "api_ weather".
-add_shortcode( 'api_weather', array( $ApiWeather, 'api_weather_func' ) );
+add_shortcode('api_weather', array($ApiWeather, 'api_weather_func'));
 
 //enque the scripts, so they are included at the right time.
-add_action( 'wp_enqueue_scripts', array( $ApiWeather, 'api_weather_enqueue_scripts' ) );
+add_action('wp_enqueue_scripts', array($ApiWeather, 'api_weather_enqueue_scripts'));
